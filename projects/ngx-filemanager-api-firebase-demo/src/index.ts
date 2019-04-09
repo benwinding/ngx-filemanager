4ddfedf3615cae343511e4b105c07db1fa49a82c
Ben Winding <ben.winding@gmail.com>
2019-04-09 16:36:19 +0930

    Remove useless dependencies on bucketname and projectId

--- projects/ngx-filemanager-api-firebase-demo/src/index.ts
+++ projects/ngx-filemanager-api-firebase-demo/src/index.ts
@@ -11,20 +11,14 @@ app.use(LogRequest);
 import * as admin from 'firebase-admin';
 // Setup local firebase admin, using service account credentials
 const serviceAccount = require('../../../../serviceAccountKey.json');
-const testbucketname = 'resvu-integration-tests.appspot.com';
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  projectId: 'resvu-integration-tests',
-  storageBucket: testbucketname
+  credential: admin.credential.cert(serviceAccount)
 });
 
 import { FileManagerEndpointExpress } from '../../ngx-filemanager-api-firebase/src/public_api';
-app.use('/api_files/', FileManagerEndpointExpress(admin.storage()));
+app.use('/ApiPublic/files/', FileManagerEndpointExpress(admin.storage()));
 
 const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
   console.log('server listening on PORT=' + PORT);
-
-  const routePaths = app._router.stack.map(r => r.name); // registered routes
-  console.log('configured routes: ', routePaths);
 });
