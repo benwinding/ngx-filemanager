1681b8f144690127f939f227a331aa426ad3b7c2
Ben Winding <ben.winding@gmail.com>
2019-04-17 16:08:41 +0930

    Fixed endpoint name

--- projects/ngx-filemanager-client-firebase-demo/src/app/test-functions-locally.component.ts
+++ projects/ngx-filemanager-client-firebase-demo/src/app/test-functions-locally.component.ts
@@ -42,7 +42,7 @@ export class AppTestFunctionsLocallyComponent {
     groups: $groups,
   };
   constructor(public firebaseClientProvider: ServerFilesystemProviderService) {
-    const bucketName = 'resvu-integration-tests.appspot.comsssss';
+    const bucketName = 'resvu-integration-tests.appspot.com';
     const apiEndpoint = 'http://localhost:4444/ApiPublic/files';
     // const bucketName = 'cl-building-storage';
     // const apiEndpoint =
